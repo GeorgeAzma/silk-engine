18cd82090c623ac1ba87c417d5b0928ef14aca1d
GeorgeAzma <gg20060705@gmail.com>
2024-01-04 13:37:27 +0400

    _

--- src/app/simple_app.rs
+++ src/app/simple_app.rs
@@ -27,19 +27,5 @@ impl SimpleApp {
 
     pub fn event(&mut self, _event: &WindowEvent) {}
 
-    pub fn render(&mut self, gfx: &mut renderer::Renderer) {
-        // gfx.rotation = self.time.as_secs_f32();
-        gfx.color = [255, 255, 255, 32];
-        gfx.lines(
-            &[
-                self.mouse_x,
-                self.mouse_y,
-                self.mouse_x * (self.mouse_left as i32 as f32),
-                self.mouse_y * (self.mouse_left as i32 as f32),
-                1.0,
-                0.0,
-            ],
-            0.1,
-        );
-    }
+    pub fn render(&mut self, gfx: &mut renderer::Renderer) {}
 }
