bd9f0766a0e87e985f48139faa73691b6442a41c
GeorgeAzma <lumi.main.mail@gmail.com>
2024-12-16 10:27:37 +0400

    oops

---
+++ src/gfx/vulkan/config.rs
@@ -0,0 +1,58 @@
+use std::ffi::{CStr, CString};
+
+use ash::khr;
+
+pub fn required_vulkan_instance_extensions() -> Vec<CString> {
+    [
+        khr::surface::NAME,
+        khr::get_surface_capabilities2::NAME,
+        #[cfg(target_os = "windows")]
+        khr::win32_surface::NAME,
+        #[cfg(target_os = "linux")]
+        khr::wayland_surface::NAME,
+        #[cfg(target_os = "macos")]
+        mvk::macos_surface::NAME,
+    ]
+    .into_iter()
+    .map(|e| e.to_owned())
+    .collect()
+}
+
+pub fn preferred_vulkan_instance_extensions() -> Vec<CString> {
+    [
+        #[cfg(debug_assertions)]
+        ash::ext::debug_utils::NAME,
+    ]
+    .into_iter()
+    .map(|e: &CStr| e.to_owned())
+    .collect()
+}
+
+pub fn enabled_layers() -> Vec<CString> {
+    [
+        // FOR SOME FUCKING REASON VALIDATION FIXES BLACK SCREEN BUT DOES NOT ERROR
+        // #[cfg(debug_assertions)]
+        "VK_LAYER_KHRONOS_validation",
+    ]
+    .into_iter()
+    .map(|e: &str| CString::new(e).unwrap())
+    .collect()
+}
+
+pub fn required_vulkan_gpu_extensions() -> Vec<CString> {
+    [khr::swapchain::NAME]
+        .into_iter()
+        .map(|e| e.to_owned())
+        .collect()
+}
+
+pub fn preferred_vulkan_gpu_extensions() -> Vec<CString> {
+    [
+        // khr::draw_indirect_count::NAME,
+        #[cfg(debug_assertions)]
+        khr::pipeline_executable_properties::NAME,
+    ]
+    .into_iter()
+    .map(|e: &CStr| e.to_owned())
+    .collect()
+}
